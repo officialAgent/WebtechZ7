d6154c28e047d6e5f6aa388f7bd2c842fb91a016
officialAgent <61824643+officialAgent@users.noreply.github.com>
2020-11-19 19:42:55 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    $.getJSON('items.json', function(data) {
+        $.each(data.items, function(i, f) {
+            $("#images").append("<img  src=" + f.src + " >");
+
+        });
+    });
+});
+
+
+(function($) {
+    $(document).ready(function() {
+        $("#inputT").keyup(function() {
+
+            // Retrieve the input field text and reset the count to zero
+            escape = function(text) {
+                return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+            };
+
+            var filter = escape($(this).val());
+
+            $('#images img').remove();
+
+            if (filter === ''){
+                $.getJSON('items.json', function(data) {
+                    $.each(data.items, function(i, f) {
+                        $("#images").append("<img src=" + f.src + " >");
+
+                    });
+                });
+            }
+            else {
+
+
+            $.getJSON('items.json', function(data) {
+                $.each(data.items, function(i, f) {
+                    if (f.title.includes(filter) || f.description.includes(filter)){
+                        $("#images").append("<img src=" + f.src + " >");
+                    }
+                    else {
+
+                    }
+
+                });
+            });
+            // Loop through the comment list
+
+
+            }
+        });
+    })
+})(jQuery)
+
+dragula([document.querySelector('#images')])
