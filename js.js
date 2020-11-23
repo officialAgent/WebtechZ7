7e7ef8415621fd19575b73f81c2ed6ac41fac091
officialAgent <61824643+officialAgent@users.noreply.github.com>
2020-11-23 10:13:25 +0100

    uj valtozat mobil verzio 2.0 lightbox fejlesztes elotti verzio meg a lightbox hianyzik

--- js.js
+++ js.js
@@ -1,15 +1,112 @@
 
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("sor");
+
+
+
+    if (user !== "") {
+
+    } else {
+        alert("This site use cookie");
+        user = ["1","2","3","4","5","6"];
+        setCookie("sor", user, 30);
+       kiiras();
+    }
+}
 $(document).ready(function () {
 
-    $.getJSON('items.json', function (data) {
-        $.each(data.items, function (i, f) {
-            $('#images').append("<a  href=" + f.src + " data-lightbox= \"images\" data-title=" + f.description + "   > <img  src=" + f.src + " ></a>" );
+    var kep =0;
 
+
+      var sor=getCookie("sor");
+      if (sor !== ''){
+      
+
+    var k=0;
+    $.getJSON('items.json', function (data) {
+
+        while (kep !==6) {
+            $.each(data.items, function (i, f) {
+
+                if (f.id === sor[kep+k]) {
+                    $('#images').append("<a class='kepek' id=" + f.id + "  href=" + f.src + " data-lightbox= \"images\" data-title=" + f.description + "   > <img  src=" + f.src + " ></a>");
+                    kep++;
+                    k++;
+
+
+                }
+
+
+            });
+
+
+
+
+        }
+
+    });
+      }
+});
+
+
+function kiiras () {
+
+    var kep =0;
+
+
+    var sor=getCookie("sor");
+    if (sor !== ''){
+
+
+        var k=0;
+        $.getJSON('items.json', function (data) {
+
+            while (kep !==6) {
+                $.each(data.items, function (i, f) {
+
+                    if (f.id === sor[kep+k]) {
+                        $('#images').append("<a class='kepek' id=" + f.id + "  href=" + f.src + " data-lightbox= \"images\" data-title=" + f.description + "   > <img  src=" + f.src + " ></a>");
+                        kep++;
+                        k++;
+
+
+                    }
+
+
+                });
+
+
+
+
+            }
 
         });
-    });
-});
+    }
+}
+
 
 /*
 $(document).ready(function() {
@@ -34,22 +131,17 @@ $(document).ready(function() {
 
             var filter = escape($(this).val());
 
-            $('#images img').remove();
+            $('#images a').remove();
 
             if (filter === '') {
-                $.getJSON('items.json', function (data) {
-                    $.each(data.items, function (i, f) {
-                        $("#images").append("<img src=" + f.src + " >");
-
-                    });
-                });
+               kiiras();
             } else {
 
 
                 $.getJSON('items.json', function (data) {
                     $.each(data.items, function (i, f) {
                         if (f.title.includes(filter) || f.description.includes(filter)) {
-                            $("#images").append("<img src=" + f.src + " >");
+                            $('#images').append("<a class='kepek' id=" + f.id + "  href=" + f.src + " data-lightbox= \"images\" data-title=" + f.description + "   > <img  src=" + f.src + " ></a>");
                         } else {
 
                         }
@@ -65,5 +157,17 @@ $(document).ready(function() {
 })(jQuery)
 
 dragula([document.querySelector('#images')])
+    .on("dragend", function(el, target, src) {
+        elems = []; // reset elems
+        $('.kepek').each(function(i, obj) {
+            //test
+            elems.push(obj.id);
+
+        });
+
+        setCookie("sor", elems, 30);
+        // validate that elems are in correct order
+        // ...
+    });
 
 
